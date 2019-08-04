e168b2f33a97d7c9d26bccd07136006d12f5b363
Samuel Holland <samuel@sholland.org>
2019-08-04 15:34:09 -0500

    Alpha UAC policy sysctl

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -160,6 +160,11 @@ static unsigned long hung_task_timeout_max = (LONG_MAX/HZ);
 #ifdef CONFIG_INOTIFY_USER
 #include <linux/inotify.h>
 #endif
+
+#ifdef CONFIG_ALPHA_UAC_SYSCTL
+extern struct ctl_table uac_table[];
+#endif
+
 #ifdef CONFIG_SPARC
 #endif
 
@@ -1973,6 +1978,13 @@ static struct ctl_table debug_table[] = {
 		.extra2		= SYSCTL_ONE,
 	},
 #endif
+#ifdef CONFIG_ALPHA_UAC_SYSCTL
+	{
+	        .procname   = "uac",
+		.mode       = 0555,
+	        .child      = uac_table,
+	 },
+#endif /* CONFIG_ALPHA_UAC_SYSCTL */
 	{ }
 };
 
