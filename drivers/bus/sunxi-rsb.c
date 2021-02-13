054762142f913e86518b741cad090d4ed6187e24
Samuel Holland <samuel@sholland.org>
2021-02-12 23:46:21 -0600

    Revert "Merge branch 'patch/rsb-extra' into pine64"
    This reverts commit a557a6cce1aa9ec95a8aa933ad4c9bebbf674e95, reversing
    changes made to 2b4eca5f69b8c9ed7dd000b3a69a6421193fd71f.

--- drivers/bus/sunxi-rsb.c
+++ drivers/bus/sunxi-rsb.c
@@ -278,6 +278,8 @@ static int _sunxi_rsb_run_xfer(struct sunxi_rsb *rsb)
 
 	reinit_completion(&rsb->complete);
 
+	writel(RSB_INTS_LOAD_BSY | RSB_INTS_TRANS_ERR | RSB_INTS_TRANS_OVER,
+	       rsb->regs + RSB_INTE);
 	writel(RSB_CTRL_START_TRANS | RSB_CTRL_GLOBAL_INT_ENB,
 	       rsb->regs + RSB_CTRL);
 
@@ -285,7 +287,7 @@ static int _sunxi_rsb_run_xfer(struct sunxi_rsb *rsb)
 					    msecs_to_jiffies(100))) {
 		dev_dbg(rsb->dev, "RSB timeout\n");
 
-		/* abort the transfer and disable interrupts */
+		/* abort the transfer */
 		writel(RSB_CTRL_ABORT_TRANS, rsb->regs + RSB_CTRL);
 
 		/* clear any interrupt flags */
@@ -314,26 +316,29 @@ static int _sunxi_rsb_run_xfer(struct sunxi_rsb *rsb)
 	return 0;
 }
 
-/* RSB regmap functions */
-struct sunxi_rsb_ctx {
-	struct sunxi_rsb_device *rdev;
-	u32 mask;
-	u8 rd_cmd;
-	u8 wr_cmd;
-};
-
-static int regmap_sunxi_rsb_reg_read(void *context, unsigned int reg,
-				     unsigned int *val)
+static int sunxi_rsb_read(struct sunxi_rsb *rsb, u8 rtaddr, u8 addr,
+			  u32 *buf, size_t len)
 {
-	struct sunxi_rsb_ctx *ctx = context;
-	struct sunxi_rsb_device *rdev = ctx->rdev;
-	struct sunxi_rsb *rsb = rdev->rsb;
+	u32 cmd;
 	int ret;
 
-	if (!val)
+	if (!buf)
 		return -EINVAL;
-	if (reg > 0xff)
+
+	switch (len) {
+	case 1:
+		cmd = RSB_CMD_RD8;
+		break;
+	case 2:
+		cmd = RSB_CMD_RD16;
+		break;
+	case 4:
+		cmd = RSB_CMD_RD32;
+		break;
+	default:
+		dev_err(rsb->dev, "Invalid access width: %zd\n", len);
 		return -EINVAL;
+	}
 
 	ret = pm_runtime_resume_and_get(rsb->dev);
 	if (ret)
@@ -341,15 +346,15 @@ static int regmap_sunxi_rsb_reg_read(void *context, unsigned int reg,
 
 	mutex_lock(&rsb->lock);
 
-	writel(reg, rsb->regs + RSB_ADDR);
-	writel(RSB_DAR_RTA(rdev->rtaddr), rsb->regs + RSB_DAR);
-	writel(ctx->rd_cmd, rsb->regs + RSB_CMD);
+	writel(addr, rsb->regs + RSB_ADDR);
+	writel(RSB_DAR_RTA(rtaddr), rsb->regs + RSB_DAR);
+	writel(cmd, rsb->regs + RSB_CMD);
 
 	ret = _sunxi_rsb_run_xfer(rsb);
 	if (ret)
 		goto unlock;
 
-	*val = readl(rsb->regs + RSB_DATA) & ctx->mask;
+	*buf = readl(rsb->regs + RSB_DATA) & GENMASK(len * 8 - 1, 0);
 
 unlock:
 	mutex_unlock(&rsb->lock);
@@ -360,16 +365,29 @@ unlock:
 	return ret;
 }
 
-static int regmap_sunxi_rsb_reg_write(void *context, unsigned int reg,
-				      unsigned int val)
+static int sunxi_rsb_write(struct sunxi_rsb *rsb, u8 rtaddr, u8 addr,
+			   const u32 *buf, size_t len)
 {
-	struct sunxi_rsb_ctx *ctx = context;
-	struct sunxi_rsb_device *rdev = ctx->rdev;
-	struct sunxi_rsb *rsb = rdev->rsb;
+	u32 cmd;
 	int ret;
 
-	if (reg > 0xff)
+	if (!buf)
 		return -EINVAL;
+
+	switch (len) {
+	case 1:
+		cmd = RSB_CMD_WR8;
+		break;
+	case 2:
+		cmd = RSB_CMD_WR16;
+		break;
+	case 4:
+		cmd = RSB_CMD_WR32;
+		break;
+	default:
+		dev_err(rsb->dev, "Invalid access width: %zd\n", len);
+		return -EINVAL;
+	}
 
 	ret = pm_runtime_resume_and_get(rsb->dev);
 	if (ret)
@@ -377,11 +395,10 @@ static int regmap_sunxi_rsb_reg_write(void *context, unsigned int reg,
 
 	mutex_lock(&rsb->lock);
 
-	writel(reg, rsb->regs + RSB_ADDR);
-	writel(RSB_DAR_RTA(rdev->rtaddr), rsb->regs + RSB_DAR);
-	writel(val, rsb->regs + RSB_DATA);
-	writel(ctx->wr_cmd, rsb->regs + RSB_CMD);
-
+	writel(addr, rsb->regs + RSB_ADDR);
+	writel(RSB_DAR_RTA(rtaddr), rsb->regs + RSB_DAR);
+	writel(*buf, rsb->regs + RSB_DATA);
+	writel(cmd, rsb->regs + RSB_CMD);
 	ret = _sunxi_rsb_run_xfer(rsb);
 
 	mutex_unlock(&rsb->lock);
@@ -390,6 +407,33 @@ static int regmap_sunxi_rsb_reg_write(void *context, unsigned int reg,
 	pm_runtime_put_autosuspend(rsb->dev);
 
 	return ret;
+}
+
+/* RSB regmap functions */
+struct sunxi_rsb_ctx {
+	struct sunxi_rsb_device *rdev;
+	int size;
+};
+
+static int regmap_sunxi_rsb_reg_read(void *context, unsigned int reg,
+				     unsigned int *val)
+{
+	struct sunxi_rsb_ctx *ctx = context;
+	struct sunxi_rsb_device *rdev = ctx->rdev;
+
+	if (reg > 0xff)
+		return -EINVAL;
+
+	return sunxi_rsb_read(rdev->rsb, rdev->rtaddr, reg, val, ctx->size);
+}
+
+static int regmap_sunxi_rsb_reg_write(void *context, unsigned int reg,
+				      unsigned int val)
+{
+	struct sunxi_rsb_ctx *ctx = context;
+	struct sunxi_rsb_device *rdev = ctx->rdev;
+
+	return sunxi_rsb_write(rdev->rsb, rdev->rtaddr, reg, &val, ctx->size);
 }
 
 static void regmap_sunxi_rsb_free_ctx(void *context)
@@ -411,24 +455,13 @@ static struct sunxi_rsb_ctx *regmap_sunxi_rsb_init_ctx(struct sunxi_rsb_device *
 		const struct regmap_config *config)
 {
 	struct sunxi_rsb_ctx *ctx;
-	u8 rd_cmd, wr_cmd;
 
 	switch (config->val_bits) {
 	case 8:
-		rd_cmd = RSB_CMD_RD8;
-		wr_cmd = RSB_CMD_WR8;
-		break;
 	case 16:
-		rd_cmd = RSB_CMD_RD16;
-		wr_cmd = RSB_CMD_WR16;
-		break;
 	case 32:
-		rd_cmd = RSB_CMD_RD32;
-		wr_cmd = RSB_CMD_WR32;
 		break;
 	default:
-		dev_err(&rdev->dev, "Invalid RSB access width: %d\n",
-			config->val_bits);
 		return ERR_PTR(-EINVAL);
 	}
 
@@ -437,9 +470,7 @@ static struct sunxi_rsb_ctx *regmap_sunxi_rsb_init_ctx(struct sunxi_rsb_device *
 		return ERR_PTR(-ENOMEM);
 
 	ctx->rdev = rdev;
-	ctx->mask = GENMASK(config->val_bits - 1, 0);
-	ctx->rd_cmd = rd_cmd;
-	ctx->wr_cmd = wr_cmd;
+	ctx->size = config->val_bits / 8;
 
 	return ctx;
 }
@@ -468,8 +499,7 @@ static irqreturn_t sunxi_rsb_irq(int irq, void *dev_id)
 	status = readl(rsb->regs + RSB_INTS);
 	rsb->status = status;
 
-	/* Disable and clear interrupts */
-	writel(0, rsb->regs + RSB_CTRL);
+	/* Clear interrupts */
 	status &= (RSB_INTS_LOAD_BSY | RSB_INTS_TRANS_ERR |
 		   RSB_INTS_TRANS_OVER);
 	writel(status, rsb->regs + RSB_INTS);
@@ -649,13 +679,6 @@ static int sunxi_rsb_hw_init(struct sunxi_rsb *rsb)
 	writel(RSB_CCR_SDA_OUT_DELAY(clk_delay) | RSB_CCR_CLK_DIV(clk_div - 1),
 	       rsb->regs + RSB_CCR);
 
-	/*
-	 * Select the interrupts we care about. They will not actually trigger
-	 * until the RSB_CTRL_GLOBAL_INT_ENB bit is set.
-	 */
-	writel(RSB_INTS_LOAD_BSY | RSB_INTS_TRANS_ERR | RSB_INTS_TRANS_OVER,
-	       rsb->regs + RSB_INTE);
-
 	return 0;
 
 err_clk_disable:
@@ -709,6 +732,7 @@ static int sunxi_rsb_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
+	struct resource *r;
 	struct sunxi_rsb *rsb;
 	u32 clk_freq = 3000000;
 	int irq, ret;
@@ -728,8 +752,8 @@ static int sunxi_rsb_probe(struct platform_device *pdev)
 	rsb->dev = dev;
 	rsb->clk_freq = clk_freq;
 	platform_set_drvdata(pdev, rsb);
-
-	rsb->regs = devm_platform_ioremap_resource(pdev, 0);
+	r = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	rsb->regs = devm_ioremap_resource(dev, r);
 	if (IS_ERR(rsb->regs))
 		return PTR_ERR(rsb->regs);
 
