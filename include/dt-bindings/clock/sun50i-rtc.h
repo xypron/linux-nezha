08bae44798880e22a4b20aad3cf52b211ebb5fd6
Samuel Holland <samuel@sholland.org>
2021-05-20 00:08:20 -0500

    clk: sunxi-ng: Add support for newer sun50i RTCs
    Signed-off-by: Samuel Holland <samuel@sholland.org>

---
+++ include/dt-bindings/clock/sun50i-rtc.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef _DT_BINDINGS_CLK_SUN50I_RTC_CCU_H_
+#define _DT_BINDINGS_CLK_SUN50I_RTC_CCU_H_
+
+#define CLK_OSC32K		0
+#define CLK_RTC_32K_FANOUT	1
+#define CLK_IOSC		2
+#define CLK_DCXO24M		3
+
+#define CLK_RTC_1K		8
+#define CLK_RTC_SPI		9
+
+#endif /* _DT_BINDINGS_CLK_SUN50I_RTC_CCU_H_ */
